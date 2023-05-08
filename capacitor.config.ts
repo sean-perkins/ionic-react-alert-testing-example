6351eb7d9fa45fa4bfe6b4d0687ddf630808dce4
Sean Perkins <sean@ionic.io>
2023-05-08 12:44:30 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'react-testing-alert',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
